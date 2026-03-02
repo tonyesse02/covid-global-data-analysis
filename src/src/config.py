761e8c19b7c96bee4b1617909d4c5a7f683d053b
Antonio Spagnuolo <129113648+tonyesse02@users.noreply.github.com>
2026-03-02 18:30:28 +0100

    Delete src/src/config.py

--- src/src/config.py
+++
@@ -1,4 +0,0 @@
-OWID_URL = "https://raw.githubusercontent.com/owid/covid-19-data/refs/heads/master/public/data/owid-covid-data.csv"
-
-DEFAULT_COUNTRIES = ["Italy", "France", "Germany", "Spain"]
-DEFAULT_YEAR_RANGE = (2020, 2025)
