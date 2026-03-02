d04587752875249006ab937625ad9a8381f7d68c
Antonio Spagnuolo <129113648+tonyesse02@users.noreply.github.com>
2026-03-02 18:34:27 +0100

    Add configuration for OWID data and default settings

---
+++ src/config.py
@@ -0,0 +1,4 @@
+OWID_URL = "https://raw.githubusercontent.com/owid/covid-19-data/refs/heads/master/public/data/owid-covid-data.csv"
+
+DEFAULT_COUNTRIES = ["Italy", "France", "Germany", "Spain"]
+DEFAULT_YEAR_RANGE = (2020, 2025)
